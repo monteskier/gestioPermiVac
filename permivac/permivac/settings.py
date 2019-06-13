107ce55558affe896e71b3e0beb5423b2c7244f1
monteskier <jmontoliu7@gmail.com>
2019-06-13 15:57:21 +0200

    mes arranjaments sobre static

--- permivac/permivac/settings.py
+++ permivac/permivac/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/tramitador/static/'
 MEDIA_URL = '/documents/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'tramitador/static/documents/upload')
 
