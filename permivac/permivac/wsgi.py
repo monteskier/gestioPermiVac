ea1afc771d6b3e7668daa265a16edd5ef51edcf8
monteskier <jmontoliu7@gmail.com>
2019-06-13 14:59:22 +0200

    Es canvien parametres al wsgi per produxxio com les llibreries sys

--- permivac/permivac/wsgi.py
+++ permivac/permivac/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/gestioPermiVac')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'permivac.settings')
 
 application = get_wsgi_application()
