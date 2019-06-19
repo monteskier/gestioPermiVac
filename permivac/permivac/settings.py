7a16a97b65cdceb14716f16d0af941bcfe5cea6b
monteskier <jmontoliu7@gmail.com>
2019-06-19 13:53:11 +0200

    Sarregla com es veu el menu de login incial amb un nou css i es fa el gitignore del settings per productiu

--- permivac/permivac/settings.py
+++ permivac/permivac/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'permivac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'permivac',
-            'USER': 'permivac',
-            'PASSWORD': 'N0P0tsP4ss4r',
-            'HOST': 'localhost',
-            'PORT': '3306',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+#DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.mysql',
+#            'NAME': 'permivac',
+#            'USER': 'permivac',
+#            'PASSWORD': 'N0P0tsP4ss4r',
+#            'HOST': 'localhost',
+#            'PORT': '3306',
+#        }
+#    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
