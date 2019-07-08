623d962cdde69b2f08ec4987d7307b0b87f34e24
monteskier <jmontoliu7@gmail.com>
2019-07-08 13:15:08 +0200

    Sha creat un fitxer script.py per crear els calendaris dels usuarias que no en tenen del any actual versio 2

--- permivac/script.py
+++ permivac/script.py
@@ -13,7 +13,7 @@ def create_calendar():
             calendari = Calendari.objects.get(treballador__id = treballador.id, any=today.year)
         except Calendari.DoesNotExist:
             print("No existeix cap Calendari d'aquest any al treballador")
-            calendari = Calendari(any=int(today.year),vacances=22,vacances_dies=0,perm_precep=0,perm_precep_dies=0,perm_no_precep=0,perm_no_precep_dies=0,asum_p=0,asum_p_dies=0,treballador=treballador)
+            calendari = Calendari(any=int(today.year),vacances=22,vacances_dies=0,perm_precep=0,perm_precep_dies=0,perm_no_precep=0,perm_no_precep_dies=0,asum_p=6,asum_p_dies=0,treballador=treballador)
             calendari.save()
             count = count+1;
     print("S'han creat un total de "+str(count)+" calendaris")
