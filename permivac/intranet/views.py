11988cef31e09554c1db3c6991ecd1e132fefa87
monteskier <jmontoliu7@gmail.com>
2020-01-29 09:20:57 +0100

    Es fa un control try en el index del intranet per si el usuari no esta autentificat

--- permivac/intranet/views.py
+++ permivac/intranet/views.py
@@ -5,9 +5,12 @@ from .models import Links
 # Create your views here.
 def index(request):
     links_publics = Links.objects.all().filter(tipus='public')
-    if request.user is not None:
-        links_privats = Links.objects.all().filter(areas__in = request.user.areas.all()).exclude(tipus='public')
-        context =  {'links_publics':links_publics, 'links_privats':links_privats}
-    else:
+    try:
+        if request.user is not None:
+            links_privats = Links.objects.all().filter(areas__in = request.user.areas.all()).exclude(tipus='public')
+            context =  {'links_publics':links_publics, 'links_privats':links_privats}
+        else:
+            context = {'links_publics':links_publics}
+    except:
         context = {'links_publics':links_publics}
     return render(request, 'index.html',context)
